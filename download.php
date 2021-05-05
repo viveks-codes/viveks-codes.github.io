3272b5bc37385f39b512f9f3b0232a8186ef4e1a
vivek patel <vivolscute@gmail.com>
2021-05-05 20:57:43 +0530

    added bugs ill fix that ltr

--- download.php
+++ download.php
@@ -1,12 +1,16 @@
-<?php if (isset($_GET['file'])) {
-$file = $_GET['file'];
-if (file_exists($file) && is_readable($file) && preg_match('/\.pdf$/',$file)) {
- header('Content-Type: application/pdf');
- header("Content-Disposition: attachment; filename=\"$file\"");
- readfile($file);
-}
-} else {
- header("HTTP/1.0 404 Not Found");
- echo "<h1>Error 404: File Not Found: <br /><em>$file</em></h1>";
-}
-?>
+header("Content-Type: application/octet-stream");
+
+$file = $_GET["file"] .".pdf";
+header("Content-Disposition: attachment; filename=" . urlencode($file));   
+header("Content-Type: application/octet-stream");
+header("Content-Type: application/download");
+header("Content-Description: File Transfer");            
+header("Content-Length: " . filesize($file));
+flush(); // this doesn't really matter.
+$fp = fopen($file, "r");
+while (!feof($fp))
+{
+    echo fread($fp, 65536);
+    flush(); // this is essential for large downloads
+} 
+fclose($fp); 
